4c02179260ec1940b94be92b3ae8c31dc00f9971
Shrayan Bajracharya <shrayanbajracharya@gmail.com>
2020-04-19 22:45:09 +0545

    Created RecyclerView and Adapter to display list of Strings in MainActivity

---
+++ app/src/main/java/com/example/todo/MainAdapter.kt
@@ -0,0 +1,36 @@
+package com.example.todo
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class MainAdapter : RecyclerView.Adapter<MainAdapter.MainViewHolder>() {
+
+    private val tasks = arrayListOf<String>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_task, parent, false)
+        return MainViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+        holder.taskTitle.text = tasks[position]
+    }
+
+    override fun getItemCount(): Int {
+        return tasks.size
+    }
+
+    fun update(tasks: List<String>) {
+        this.tasks.clear()
+        this.tasks.addAll(tasks)
+        notifyDataSetChanged()
+    }
+
+    class MainViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val taskTitle: TextView = itemView.findViewById(R.id.tv_task_title)
+    }
+}
