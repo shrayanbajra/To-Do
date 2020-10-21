2d7452c42f8df1c3e3232f260f8577862a13b363
Shrayan Bajracharya <shrayanbajracharya@gmail.com>
2020-10-21 21:20:03 +0545

    Refactored code

--- app/src/main/java/com/example/todo/ui/addtask/AddTaskRepository.kt
+++ app/src/main/java/com/example/todo/ui/addtask/AddTaskRepository.kt
@@ -2,8 +2,6 @@ package com.example.todo.ui.addtask
 
 import com.example.todo.db.task.TaskEntity
 import com.example.todo.ui.BaseRepository
-import kotlinx.coroutines.Dispatchers.IO
-import kotlinx.coroutines.withContext
 
 class AddTaskRepository private constructor() : BaseRepository() {
 
@@ -18,8 +16,6 @@ class AddTaskRepository private constructor() : BaseRepository() {
     }
 
     suspend fun insertTask(task: TaskEntity) {
-        withContext(IO) {
-            taskDao.insert(task)
-        }
+        taskDao.insert(task)
     }
 }
--- app/src/main/java/com/example/todo/ui/addtask/AddTaskViewModel.kt
+++ app/src/main/java/com/example/todo/ui/addtask/AddTaskViewModel.kt
@@ -3,14 +3,14 @@ package com.example.todo.ui.addtask
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.todo.db.task.TaskEntity
+import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.launch
 
 class AddTaskViewModel : ViewModel() {
 
-    private val repository = AddTaskRepository.getInstance()
-
     fun insertTask(task: TaskEntity) {
-        viewModelScope.launch {
+        viewModelScope.launch(IO) {
+            val repository = AddTaskRepository.getInstance()
             repository.insertTask(task)
         }
     }
