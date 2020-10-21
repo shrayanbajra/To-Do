8190b6964e23e4a6ef09f17e68d83d7863b92774
Shrayan Bajracharya <shrayanbajracharya@gmail.com>
2020-10-21 12:34:49 +0545

    Added splash screen

---
+++ app/src/main/java/com/example/todo/ui/SplashScreenActivity.kt
@@ -0,0 +1,31 @@
+package com.example.todo.ui
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.example.todo.R
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        lifecycleScope.launch(Main) {
+            val twoSeconds = 2000L
+            delay(twoSeconds)
+            navigateToNavHostActivity()
+            finish()
+        }
+    }
+
+    private fun navigateToNavHostActivity() {
+        val intent = Intent(this, NavHostActivity::class.java)
+        startActivity(intent)
+    }
+
+}
