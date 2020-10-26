73fab3df07c71263626118f189d2363332a5de8e
Shrayan Bajracharya <shrayanbajracharya@gmail.com>
2020-10-26 11:38:04 +0545

    Setup Stetho

--- app/src/main/java/com/example/todo/di/BaseApplication.kt
+++ app/src/main/java/com/example/todo/di/BaseApplication.kt
@@ -2,6 +2,7 @@ package com.example.todo.di
 
 import com.example.todo.BuildConfig
 import com.example.todo.di.app.DaggerAppComponent
+import com.facebook.stetho.Stetho
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
 import timber.log.Timber
@@ -11,12 +12,12 @@ class BaseApplication : DaggerApplication() {
 
     override fun onCreate() {
         super.onCreate()
-        if (BuildConfig.DEBUG) {
-            Timber.plant(DebugTree())
-        }
+
+        Stetho.initializeWithDefaults(this)
+        if (BuildConfig.DEBUG) Timber.plant(DebugTree())
     }
 
-    override fun applicationInjector(): AndroidInjector<out DaggerApplication>? {
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         return DaggerAppComponent.builder().application(this).build()
     }
 
