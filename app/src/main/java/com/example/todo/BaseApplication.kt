535f6103e789997f3035357d8c40e33cf7b1a76c
Shrayan Bajracharya <shrayanbajracharya@gmail.com>
2020-10-26 12:18:39 +0545

    Rearranged files

---
+++ app/src/main/java/com/example/todo/BaseApplication.kt
@@ -0,0 +1,23 @@
+package com.example.todo
+
+import com.example.todo.di.app.DaggerAppComponent
+import com.facebook.stetho.Stetho
+import dagger.android.AndroidInjector
+import dagger.android.DaggerApplication
+import timber.log.Timber
+import timber.log.Timber.DebugTree
+
+class BaseApplication : DaggerApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Stetho.initializeWithDefaults(this)
+        if (BuildConfig.DEBUG) Timber.plant(DebugTree())
+    }
+
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+        return DaggerAppComponent.builder().application(this).build()
+    }
+
+}
